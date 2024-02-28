53d6f1f9fbee22d921a53b4caf079002dd20dd15
vardhan1228 <vvardhan1211@gmail.com>
2024-02-28 16:15:22 +0530

    count

---
+++ d11-count-foreach/count/variable.tf
@@ -0,0 +1,22 @@
+variable "ami" {
+description = "ami value"
+type = string
+default = "ami-0440d3b780d96b29d"
+}
+
+variable "instance_type" {
+description = "instace type value"
+type = string
+default = "t2.micro"
+}
+
+variable "key_name" {
+description = "keyname value"
+type = string
+default = "sri"
+}
+variable "tags" {
+    type = list(string)
+    default = ["koti","moji"]
+  description = "passing tags names by using count"
+}
